2b456b05c63c2d903ffd692e2156448badc34efb
zaurrs <zaurrs-af106@code.edu.az>
2024-12-16 19:33:31 +0400

    created

---
+++ React/react-base/src/App.jsx
@@ -0,0 +1,23 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Products from './components/Products'
+import Cards from './components/Cards'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>salam</h1>
+     <Header></Header>
+     <Products></Products>
+     <Cards>
+     </Cards>
+     <Footer></Footer>
+     
+    </>
+  )
+}
+
+export default App
---
+++ React/react-base/src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Card({name,description,price}) {
+  return (
+    <>
+     <div className="card" style={{ width: '18rem' }}>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdxGapLRKsiWs4CrjowmhqCf9LxYsPq3JC1g&s" className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">{name}</h5>
+      <p className="card-text">
+       {description}
+      </p>
+      <p className="card-text">
+       {price}
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </div>
+  </div></>
+  )
+}
+
+export default Card
